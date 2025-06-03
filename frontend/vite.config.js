3e92ba8a1ba317f7bc20423c2aa82e46d51cd93a
zxenonx <xenon.nemrod@gmail.com>
2025-06-03 20:41:06 +0100

    Update misc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
